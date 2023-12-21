7881a2093e6f90a5a359af754fd5f37e49542c95
irina <itcvetkova@gmail.com>
2023-12-21 19:25:07 +0300

    test1

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def main():
+    print('My first git program1')
+
+
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
